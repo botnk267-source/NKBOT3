529af75fcb57a9ff8930097690c738f28f411a61
botnk267-source <botnk267@gmail.com>
2025-08-24 10:11:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,16 +108,4 @@ try {
     logger(`Failed to load package info: ${err.message}`, "[ Error ]");
 }
 
-// Start the bot
-startBot();
-const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bot is running ✅");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
